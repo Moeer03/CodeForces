52491f2155a185732ad7b5f18b3c856a32a6c723
Moeer03 <161110786+Moeer03@users.noreply.github.com>
2025-05-17 22:32:57 +0500

    Update CodeForces 1512A.py

--- CodeForces 1512A.py
+++ CodeForces 1512A.py
@@ -1,16 +1,17 @@
 t = int(input())
-
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().split()))
-
-    # Determine the majority element from first three
-    if a[0] == a[1] or a[0] == a[2]:
+    
+    # Determine the majority value
+    if a[0] == a[1]:
+        majority = a[0]
+    elif a[0] == a[2]:
         majority = a[0]
     else:
         majority = a[1]
-
-    # Find and print the 1-based index of the unique element
+    
+    # Find and print the index (1-based) of the spy (unique number)
     for i in range(n):
         if a[i] != majority:
             print(i + 1)
