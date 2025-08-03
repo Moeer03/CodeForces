8f3a24bc6a0bbdbe8b3e42c5958ce2bbfbe57483
Moeer03 <161110786+Moeer03@users.noreply.github.com>
2025-08-03 15:11:07 +0000

    Fix/Update solution for 1829B

--- CodeForces_1829B.py
+++ CodeForces_1829B.py
@@ -1,23 +1,17 @@
 t = int(input())
+
 for _ in range(t):
     n = int(input())
-    a = str(input())
-    
-    # Calculate the sum of the array
-    s1 = 0
-    i = 0
-    while i < n:
-        j = 0
-        s = 0
-        if a[i] == '0':
-            s += 1
-            while (i + j < n):
-                if a[j] == '0':
-                    s += 1
-                    j += 1
-                else:
-                    i = j
-                    break
-            if s > s1:
-                s1 = s
-    print(s1)
+    a = list(map(int, input().split()))
+
+    max_blank = 0
+    current_blank = 0
+
+    for num in a:
+        if num == 0:
+            current_blank += 1
+            max_blank = max(max_blank, current_blank)
+        else:
+            current_blank = 0
+
+    print(max_blank)
